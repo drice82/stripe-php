057a1bed9d391e9bc8e699ccfc346e26e647ee36
Remi Jannel <remi@stripe.com>
2017-11-13 15:16:57 -0500

    Move ApplePayDomain to stripe-mock

--- tests/ApplePayDomainTest.php
+++ tests/ApplePayDomainTest.php
@@ -2,71 +2,51 @@
 
 namespace Stripe;
 
-class ApplePayDomainTest extends TestCase
+define('TEST_RESOURCE_ID', 'apwc_123');
+
+class ApplePayDomainTest extends StripeMockTestCase
 {
-    public function testCreation()
+    public function testIsListable()
     {
-        $this->mockRequest(
-            'POST',
-            '/v1/apple_pay/domains',
-            array('domain_name' => 'test.com'),
-            array(
-                'id' => 'apwc_create',
-                'object' => 'apple_pay_domain'
-            )
+        $this->expectsRequest(
+            'get',
+            '/v1/apple_pay/domains'
         );
-        $d = ApplePayDomain::create(array(
-            'domain_name' => 'test.com'
+        $resources = ApplePayDomain::all();
+        $this->assertTrue(is_array($resources->data));
+        $this->assertSame("Stripe\\ApplePayDomain", get_class($resources->data[0]));
+    }
+
+    public function testIsRetrievable()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v1/apple_pay/domains/' . TEST_RESOURCE_ID
+        );
+        $resource = ApplePayDomain::retrieve(TEST_RESOURCE_ID);
+        $this->assertSame("Stripe\\ApplePayDomain", get_class($resource));
+    }
+
+    public function testIsCreatable()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v1/apple_pay/domains'
+        );
+        $resource = ApplePayDomain::create(array(
+            "domain_name" => "domain",
         ));
-        $this->assertSame('apwc_create', $d->id);
-        $this->assertInstanceOf('Stripe\\ApplePayDomain', $d);
+        $this->assertSame("Stripe\\ApplePayDomain", get_class($resource));
     }
 
-    public function testRetrieve()
+    public function testIsDeletable()
     {
-        $this->mockRequest(
-            'GET',
-            '/v1/apple_pay/domains/apwc_retrieve',
-            array(),
-            array(
-                'id' => 'apwc_retrieve',
-                'object' => 'apple_pay_domain'
-            )
+        $resource = ApplePayDomain::retrieve(TEST_RESOURCE_ID);
+        $this->expectsRequest(
+            'delete',
+            '/v1/apple_pay/domains/' . TEST_RESOURCE_ID
         );
-        $d = ApplePayDomain::retrieve('apwc_retrieve');
-        $this->assertSame('apwc_retrieve', $d->id);
-        $this->assertInstanceOf('Stripe\\ApplePayDomain', $d);
-    }
-
-    public function testDeletion()
-    {
-        self::authorizeFromEnv();
-        $d = ApplePayDomain::create(array(
-            'domain_name' => 'jackshack.website'
-        ));
-        $this->assertInstanceOf('Stripe\\ApplePayDomain', $d);
-        $this->mockRequest(
-            'DELETE',
-            '/v1/apple_pay/domains/' . $d->id,
-            array(),
-            array('deleted' => true)
-        );
-        $d->delete();
-        $this->assertTrue($d->deleted);
-    }
-
-    public function testList()
-    {
-        $this->mockRequest(
-            'GET',
-            '/v1/apple_pay/domains',
-            array(),
-            array(
-                'url' => '/v1/apple_pay/domains',
-                'object' => 'list'
-            )
-        );
-        $all = ApplePayDomain::all();
-        $this->assertSame($all->url, '/v1/apple_pay/domains');
+        $resource->delete();
+        $this->assertSame("Stripe\\ApplePayDomain", get_class($resource));
     }
 }
